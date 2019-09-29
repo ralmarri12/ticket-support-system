024b85908b5b009d8d936a6104ba23fce6625c1c
Rashid F <r.almarri12@gmail.com>
2019-09-29 20:05:41 +0800

    Clean up whitespacess from main route

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,27 +2,18 @@ const Router = require("express").Router();
 
 // #region (Authentication Routes)
 const { login } = require("../controllers/auth.controller");
-Router.post("/login", login); 
+Router.post("/login", login);
 
-const { register } = require("../controllers/auth.controller"); 
-Router.post("/register", register); 
+const { register } = require("../controllers/auth.controller");
+Router.post("/register", register);
+// #endregion (Authentication Routes)
 
-
-// #endregion (Authentication Routes) 
-
-
-//#region ( Ticket routes) 
-
-const { getTickets } = require("../controllers/tickets.controller"); 
-Router.get("/getTickets", getTickets);  
-
+//#region ( Ticket routes)
+const { getTickets } = require("../controllers/tickets.controller");
+Router.get("/getTickets", getTickets);
 
 const { addTicket } = require("../controllers/tickets.controller");
-Router.post("/addTicket", addTicket); 
-
-
+Router.post("/addTicket", addTicket);
 //#endregion (Ticket routes)
 
-
-module.exports = Router; 
- 
+module.exports = Router;

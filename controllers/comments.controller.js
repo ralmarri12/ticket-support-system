efd7ef8a6bec72af87ade38566351d25f4c61cfb
Hasan Faris <hasankhiro1993@gmail.com>
2019-10-05 21:05:54 +0800

    fix conflict in commnets controller and complete Get ticket with comments

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,15 +1,5 @@
 const CommentModel = require("../models").Comment;
 
-const getComments = async (req, res) => { 
-
-  try { 
-    const result = getCommentsByTicketID(req.param.t_id); 
-    return res.sendSuccess(result);
-    } catch(error) {
-      return res.sendError(error)
-    }
-};
-
 const postComment = async (req, res) => {
   const u_id = req.body.u_id;
   const t_id = req.body.t_id;
@@ -30,4 +20,4 @@ const postComment = async (req, res) => {
   });
 };
 
-module.exports = { getComments, postComment };
+module.exports = { getCommentsByTicketID, postComment };

4544220b72a94c48e9f4f8419250d49b92840c58
Hasan Faris <hasankhiro1993@gmail.com>
2019-11-04 22:23:51 +0800

    add user data to comment

--- helpers/comments.helper.js
+++ helpers/comments.helper.js
@@ -1,11 +1,12 @@
 const CommentModel = require('../models').Comment;  
-
+const UserModel = require('../models/user')
 const getCommentsByTicketID = async (t_id) => {
     
     return await CommentModel.findAll({
         where: {
             t_id
-        }
+        },
+        include: UserModel
     });
 } 
 

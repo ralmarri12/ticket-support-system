944d2443951bad8497a7fe08aced87945543603f
Rashid F <r.almarri12@gmail.com>
2019-11-05 21:51:56 +0800

    Modify config.js production and test environment

--- config/config.js
+++ config/config.js
@@ -13,21 +13,23 @@ module.exports = {
     }
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
+    logging: false,
     define: {
       underscored: true
     }
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
+    logging: false,
     define: {
       underscored: true
     }

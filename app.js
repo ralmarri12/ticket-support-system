5b274bf3e998323cda31af128560b952be185767
Rashid F <r.almarri12@gmail.com>
2019-11-11 20:29:12 +0800

    Add access origin to environment variable

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://tss-frontend.herokuapp.com"
-  );
+  res.header("Access-Control-Allow-Origin", process.env.ALLOW_ACCESS_ORIGIN);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
